a3263dc8bd25cae536ce4a673b52a2388f6c4cbc
kai-h-8 <kai.yy.houston@gmail.com>
2025-12-05 14:11:20 -0500

    AoC 2025 Dec4

---
+++ 2025/Dec4/Dec4-1.py
@@ -0,0 +1,20 @@
+input = open("Dec4Input.txt", "r")
+grid, adjacent, Part1 = [], [[0, 1], [0, -1], [1, 1], [1, 0], [1, -1], [-1, 1], [-1, 0], [-1, -1]], 0
+
+def adjacent_roll(grid, x, y): 
+    return x in range(len(grid)) and y in range(len(grid[x])) and grid[x][y] == "@"
+
+def accessible(grid, adjacent, x, y): 
+    count = 0
+    for adj in adjacent: 
+        if adjacent_roll(grid, x + adj[0], y + adj[1]): count += 1
+    return count < 4
+
+for i in input: 
+    grid.append(i.replace("\n", ""))
+
+for x in range(len(grid)): 
+    for y in range(len(grid[x])): 
+        if grid[x][y] == "@" and accessible(grid, adjacent, x, y): Part1 += 1
+
+print("Part 1: " + str(Part1))
---
+++ 2025/Dec4/Dec4-2.py
@@ -0,0 +1,24 @@
+input = open("Dec4Input.txt", "r")
+grid, adjacent, rolls, x, y, Part2 = [], [[0, 1], [0, -1], [1, 1], [1, 0], [1, -1], [-1, 1], [-1, 0], [-1, -1]], True, 0, 0, 0
+
+def adjacent_roll(grid, x, y): 
+    return x in range(len(grid)) and y in range(len(grid[x])) and grid[x][y] == "@"
+
+def accessible(grid, adjacent, x, y): 
+    count = 0
+    for adj in adjacent: 
+        if adjacent_roll(grid, x + adj[0], y + adj[1]): count += 1
+    return count < 4
+
+for i in input: 
+    grid.append(i.replace("\n", ""))
+
+while rolls == True: 
+    rolls = False
+    for x in range(len(grid)): 
+        for y in range(len(grid[x])): 
+            if grid[x][y] == "@" and accessible(grid, adjacent, x, y): 
+                Part2 += 1
+                grid[x], rolls = grid[x][:y] + "." + grid[x][y + 1:], True
+
+print("Part 2: " + str(Part2))
